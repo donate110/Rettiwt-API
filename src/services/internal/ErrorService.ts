4111a1dbc5e99a47c84a58de18eca18143de7a3f
Rishikant181 <rishikantsahu181@gmail.com>
2025-04-25 01:19:57 +0530

    Added raw type for error response

--- src/services/internal/ErrorService.ts
+++ src/services/internal/ErrorService.ts
@@ -1,11 +1,5 @@
-import { AxiosError, AxiosResponse, isAxiosError } from 'axios';
+import { AxiosError, isAxiosError } from 'axios';
 
-import { EApiErrors, EErrorCodes } from '../../enums/Api';
-import { EHttpStatus } from '../../enums/Http';
-import { findKeyByValue } from '../../helper/JsonUtils';
-import { ApiError } from '../../models/errors/ApiError';
-import { HttpError } from '../../models/errors/HttpError';
-import { TimeoutError } from '../../models/errors/TimeoutError';
 import { IErrorHandler } from '../../types/ErrorHandler';
 
 /**
@@ -14,130 +8,12 @@ import { IErrorHandler } from '../../types/ErrorHandler';
  * @public
  */
 export class ErrorService implements IErrorHandler {
-	/**
-	 * Error message used when the specific error type is not defined in the required enums.
-	 */
-	protected static readonly DEFAULT_ERROR_MESSAGE = 'Unknown error';
-
-	/**
-	 * Creates an API error instance based on the provided error code.
-	 *
-	 * @param errorCode - The error code.
-	 *
-	 * @returns An API error instance.
-	 */
-	protected createApiError(errorCode: number): ApiError {
-		return new ApiError(errorCode, this.getApiErrorMessage(errorCode));
+	private handleAxiosError(error: AxiosError): void {
+		throw new Error(error.message);
 	}
 
-	/**
-	 * Creates an HTTP error instance based on the provided HTTP status.
-	 *
-	 * @param httpStatus - The HTTP status code.
-	 *
-	 * @returns An HTTP error instance.
-	 */
-	protected createHttpError(httpStatus: number): HttpError {
-		return new HttpError(httpStatus, this.getHttpErrorMessage(httpStatus));
-	}
-
-	/**
-	 * Retrieves the API error message based on the provided error code.
-	 *
-	 * @param errorCode - The error code.
-	 *
-	 * @returns The API error message.
-	 */
-	protected getApiErrorMessage(errorCode: number): string {
-		const errorCodeKey = findKeyByValue(EErrorCodes, errorCode.toString());
-
-		return !!errorCodeKey && errorCodeKey in EApiErrors
-			? EApiErrors[errorCodeKey as keyof typeof EApiErrors]
-			: ErrorService.DEFAULT_ERROR_MESSAGE;
-	}
-
-	/**
-	 * Retrieves the response data from the given error.
-	 *
-	 * @param error - The error object.
-	 *
-	 * @returns The response data.
-	 *
-	 * @throws The original error if it is not an HTTP error with a response.
-	 */
-	protected getAxiosResponse(error: AxiosError): AxiosResponse {
-		if (error.response) {
-			return error.response;
-		}
-
-		throw error;
-	}
-
-	/**
-	 * Retrieves the API error code from the Axios response data.
-	 *
-	 * @param response - The response object received.
-	 *
-	 * @returns The error code, or undefined if not found.
-	 */
-	protected getErrorCode(response: AxiosResponse): number | undefined {
-		const errors = (response.data as { errors: { code: number }[] }).errors;
-
-		return !!errors && errors.length ? errors[0].code : undefined;
-	}
-
-	/**
-	 * Retrieves the HTTP error message based on the provided HTTP status.
-	 *
-	 * @param httpStatus - The HTTP status code.
-	 *
-	 * @returns The HTTP error message.
-	 */
-	protected getHttpErrorMessage(httpStatus: number): string {
-		return Object.values(EHttpStatus).includes(httpStatus)
-			? EHttpStatus[httpStatus]
-			: ErrorService.DEFAULT_ERROR_MESSAGE;
-	}
-
-	/**
-	 * Handles API error in a response.
-	 *
-	 * @param response - The response object received.
-	 *
-	 * @throws An error with the corresponding API error message if any API-related error has occurred.
-	 */
-	protected handleApiError(response: AxiosResponse): void {
-		const errorCode = this.getErrorCode(response);
-
-		if (errorCode === undefined) {
-			return;
-		}
-
-		throw this.createApiError(errorCode);
-	}
-
-	/**
-	 * Handles HTTP error in a response.
-	 *
-	 * @param response - The response object received.
-	 *
-	 * @throws An error with the corresponding HTTP status text if any HTTP-related error has occurred.
-	 */
-	protected handleHttpError(response: AxiosResponse): void {
-		throw this.createHttpError(response.status);
-	}
-
-	/**
-	 * Handles exceeded timeout, configured in RettiwtConfig.
-	 *
-	 * @param error - The error object.
-	 *
-	 * @throws An error if the configured request timeout has been exceeded.
-	 */
-	protected handleTimeoutError(error: AxiosError): void {
-		if (error.code === 'ECONNABORTED') {
-			throw new TimeoutError(error.message);
-		}
+	private handleUnknownError(): void {
+		throw new Error('Unknown error');
 	}
 
 	/**
@@ -146,14 +22,10 @@ export class ErrorService implements IErrorHandler {
 	 * @param error - The error caught while making HTTP request to Twitter API.
 	 */
 	public handle(error: unknown): void {
-		if (!isAxiosError(error)) {
-			throw error;
+		if (isAxiosError(error)) {
+			this.handleAxiosError(error as AxiosError);
+		} else {
+			this.handleUnknownError();
 		}
-
-		this.handleTimeoutError(error);
-
-		const axiosResponse = this.getAxiosResponse(error);
-		this.handleApiError(axiosResponse);
-		this.handleHttpError(axiosResponse);
 	}
 }
---
+++ src/types/raw/base/Error.ts
@@ -0,0 +1,38 @@
+/* eslint-disable */
+
+/**
+ * Represents the raw data of an error response.
+ *
+ * @public
+ */
+export interface IError {
+	status: number;
+	statusText: string;
+	data: IErrorData;
+}
+
+export interface IErrorData {
+	errors: IErrorDetails[];
+}
+
+export interface IErrorDetails {
+	message: string;
+	extensions: IErrorExtensions;
+	code: number;
+	kind: string;
+	name: string;
+	source: string;
+	tracing: IErrorTracing;
+}
+
+export interface IErrorExtensions {
+	name: string;
+	source: string;
+	code: number;
+	kind: string;
+	tracing: IErrorTracing;
+}
+
+export interface IErrorTracing {
+	trace_id: string;
+}
