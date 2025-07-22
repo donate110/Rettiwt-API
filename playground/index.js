b2848857627bb00cce4a398b16ac07a8de81f7c7
Valentin Chmara <valentinchmara@gmail.com>
2025-07-22 17:35:17 +0200

    feat: add playground

---
+++ playground/index.js
@@ -0,0 +1,18 @@
+import { Rettiwt } from 'rettiwt-api';
+import { config } from 'dotenv';
+
+config();
+
+const rettiwt = new Rettiwt({ apiKey: process.env.ACCESS_TOKEN });
+
+async function userDetails() {
+  try {
+    const user = await rettiwt.user.details();
+    console.log(user);
+  }
+  catch (error) {
+    console.error('Error fetching user details:', error);
+  }
+}
+
+await userDetails();
